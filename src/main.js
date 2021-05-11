f41e4c979bc25bbe400a7a87004bfb71d0b54e46
akhil thankachen <akhilthankachen1234@gmail.com>
2021-05-11 12:15:15 +0530

    add vue-meta

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import "@/assets/styles/tailwind.css"
 import store from './store'
 import router from './router'
+import meta from 'vue-meta'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).use(meta).mount('#app')
