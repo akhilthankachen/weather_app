c9111df29fe56a9c358f2b87c6a5552fa7e76ef8
akhil thankachen <akhilthankachen1234@gmail.com>
2021-05-11 13:01:31 +0530

    removed vue-meta

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue'
 import "@/assets/styles/tailwind.css"
 import store from './store'
 import router from './router'
-import meta from 'vue-meta'
 
-createApp(App).use(router).use(store).use(meta).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
